6bda42554ca8d905de5611c207ea87449e620102
Adi Amoyal <adi.amoyal02@gmail.com>
2023-03-08 20:09:37 +0200

    Initial Commit

---
+++ WeeatherApp/WeeatherAppApp.swift
@@ -0,0 +1,17 @@
+//
+//  WeeatherAppApp.swift
+//  WeeatherApp
+//
+//  Created by Adi Amoyal on 08/03/2023.
+//
+
+import SwiftUI
+
+@main
+struct WeeatherAppApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
